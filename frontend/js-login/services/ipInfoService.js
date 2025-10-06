b546ed4b88df24b6fb68982b5d21b7fb6b4321a9
Julio <juliophpx@gmail.com>
2025-10-06 05:35:42 -0400

    ipinfo

--- frontend/js-login/services/ipInfoService.js
+++ frontend/js-login/services/ipInfoService.js
@@ -1,5 +1,5 @@
 export function getCountryFromIP() {
-  return fetch('https://juliophp.com/API-fellow/ipinfo')
+  return fetch('https://juliophp.com/api-universal-login/ipinfo')
     .then(response => {
       if (!response.ok) {
         throw new Error('Network response was not ok');
